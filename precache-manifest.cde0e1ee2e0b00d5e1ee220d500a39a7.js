41fdbfddb4b4d80ed6160dc89933b6049253dfab
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2019-08-18 14:00:01 +0300

    Updates

---
+++ precache-manifest.cde0e1ee2e0b00d5e1ee220d500a39a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4592a9ddb7b7a4f39c1aaba084340e5c",
+    "url": "/reddit-memes/index.html"
+  },
+  {
+    "revision": "49925a6440219d3a7707",
+    "url": "/reddit-memes/static/css/main.5e19e141.chunk.css"
+  },
+  {
+    "revision": "41284167369e2d2b0ccd",
+    "url": "/reddit-memes/static/js/2.68b91d02.chunk.js"
+  },
+  {
+    "revision": "49925a6440219d3a7707",
+    "url": "/reddit-memes/static/js/main.3430402d.chunk.js"
+  },
+  {
+    "revision": "5ff3b7e5cf5f66395e97",
+    "url": "/reddit-memes/static/js/runtime~main.7a6d6e2c.js"
+  }
+]);
