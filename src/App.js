1ed82049a1e134754cf06fd5051a7146a5760b0d
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2021-04-07 23:34:23 +0300

    Small refactoring

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ class App extends Component {
 
   componentDidMount() {
     SUBREDDITS.forEach(subreddit => {
-      let url = 'https://www.reddit.com/r/' + subreddit + '.json?limit=100';
+      const url = 'https://www.reddit.com/r/' + subreddit + '.json?limit=100';
       fetch(url).then(res => res.json()).then(posts => {
         this.addToStatePosts(posts.data.children.sort(() => 0.5 - Math.random()));
       });
     });
-    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener('keydown', this.handleKeyDown);
   }
 
-  componentWillUnmount(){
-    document.removeEventListener("keydown", this.handleKeyDown);
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   addToStatePosts(newPosts) {
-    let filtered = newPosts.filter(function(value){
+    const onlyImagesPosts = newPosts.filter(value => {
       return value.data.url.match(/\.(jpeg|jpg|png)$/) != null;
     });
-    this.setState({ posts: this.state.posts.concat(filtered) });
+    this.setState({ posts: this.state.posts.concat(onlyImagesPosts) });
   }
 
   handleKeyDown(event) {
@@ -60,7 +60,7 @@ class App extends Component {
     return this.state.posts[this.state.currentPostIndex].data;
   }
 
-  setDaymode() {
+  toggleDaymode() {
     document.body.classList.toggle('daymode');
     if (document.getElementById('night-toggle').innerHTML === 'daymode')
       document.getElementById('night-toggle').innerHTML = 'nightmode';
@@ -82,7 +82,7 @@ class App extends Component {
           <div onClick={ this.prevPic } id="prev-select" className="link service-link">prev pic</div>
           <a href={ this.currentPost().url } className="link" target="_blank" rel="noopener noreferrer">direct link</a>
           <a href={ this.commentsLink() } className="link" target="_blank" rel="noopener noreferrer">comments</a>
-          <div onClick={ this.setDaymode } id="night-toggle" className="link service-link">daymode</div>
+          <div onClick={ this.toggleDaymode } id="night-toggle" className="link service-link">daymode</div>
         </div>
         <div onClick={ this.nextPic }>
           <img src={ this.currentPost().url } alt={ this.currentPost().title } style={{zIndex:-1}} />
