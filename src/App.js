9b00e94caad80217b005681f6c6c64ccea7ea8cb
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2021-12-26 03:18:20 +0200

    Add another subreddit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const SUBREDDITS = ['memes', 'me_irl', 'dankmemes'];
+const SUBREDDITS = [
+  'memes',
+  'me_irl',
+  'dankmemes',
+  'BikiniBottomTwitter'
+]
 
 class App extends Component {
   constructor(props) {
