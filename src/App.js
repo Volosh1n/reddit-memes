b9a696a2b862f7a31f965acf5fb9471e96b60a0c
Vsevolod Voloshyn <seva.voloshin@gmail.com>
2021-12-26 03:18:45 +0200

    Remove semicolons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 const SUBREDDITS = [
   'memes',
@@ -10,71 +10,71 @@ const SUBREDDITS = [
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       posts: [],
       currentPostIndex: 0
-    };
-    this.nextPic = this.nextPic.bind(this);
-    this.prevPic = this.prevPic.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.commentsLink = this.commentsLink.bind(this);
+    }
+    this.nextPic = this.nextPic.bind(this)
+    this.prevPic = this.prevPic.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.commentsLink = this.commentsLink.bind(this)
   }
 
   componentDidMount() {
     SUBREDDITS.forEach(subreddit => {
-      const url = 'https://www.reddit.com/r/' + subreddit + '.json?limit=100';
+      const url = 'https://www.reddit.com/r/' + subreddit + '.json?limit=100'
       fetch(url).then(res => res.json()).then(posts => {
-        this.addToStatePosts(posts.data.children.sort(() => 0.5 - Math.random()));
-      });
-    });
-    document.addEventListener('keydown', this.handleKeyDown);
+        this.addToStatePosts(posts.data.children.sort(() => 0.5 - Math.random()))
+      })
+    })
+    document.addEventListener('keydown', this.handleKeyDown)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
+    document.removeEventListener('keydown', this.handleKeyDown)
   }
 
   addToStatePosts(newPosts) {
     const onlyImagesPosts = newPosts.filter(value => {
-      return value.data.url.match(/\.(jpeg|jpg|png)$/) != null;
-    });
-    this.setState({ posts: this.state.posts.concat(onlyImagesPosts) });
+      return value.data.url.match(/\.(jpeg|jpg|png)$/) != null
+    })
+    this.setState({ posts: this.state.posts.concat(onlyImagesPosts) })
   }
 
   handleKeyDown(event) {
-    if (event.key === 'ArrowRight') this.nextPic();
-    if (event.key === 'ArrowLeft') this.prevPic();
+    if (event.key === 'ArrowRight') this.nextPic()
+    if (event.key === 'ArrowLeft') this.prevPic()
   }
 
   nextPic() {
     if(this.state.currentPostIndex + 1 !== this.state.posts.length)
-      this.setState({ currentPostIndex: this.state.currentPostIndex + 1 });
+      this.setState({ currentPostIndex: this.state.currentPostIndex + 1 })
     else
-      this.setState({ currentPostIndex: 0 });
+      this.setState({ currentPostIndex: 0 })
   }
 
   prevPic() {
     if(this.state.currentPostIndex - 1 >= 0)
-      this.setState({ currentPostIndex: this.state.currentPostIndex - 1 });
+      this.setState({ currentPostIndex: this.state.currentPostIndex - 1 })
     else
-      this.setState({ currentPostIndex: this.state.posts.length - 1 });
+      this.setState({ currentPostIndex: this.state.posts.length - 1 })
   }
 
   currentPost() {
-    return this.state.posts[this.state.currentPostIndex].data;
+    return this.state.posts[this.state.currentPostIndex].data
   }
 
   toggleDaymode() {
-    document.body.classList.toggle('daymode');
+    document.body.classList.toggle('daymode')
     if (document.getElementById('night-toggle').innerHTML === 'daymode')
-      document.getElementById('night-toggle').innerHTML = 'nightmode';
+      document.getElementById('night-toggle').innerHTML = 'nightmode'
     else
-      document.getElementById('night-toggle').innerHTML = 'daymode';
+      document.getElementById('night-toggle').innerHTML = 'daymode'
   }
 
   commentsLink() {
-    return 'https://reddit.com' + this.currentPost().permalink;
+    return 'https://reddit.com' + this.currentPost().permalink
   }
 
   render() {
@@ -93,8 +93,8 @@ class App extends Component {
           <img src={ this.currentPost().url } alt={ this.currentPost().title } style={{zIndex:-1}} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
